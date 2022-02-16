49bd19d3ba4296849cdb43f2d011b3458b2d048a
kittanb <vbiziukov@gmail.com>
2022-02-16 22:22:44 +0300

    fix for test #5

--- pages/projects.js
+++ pages/projects.js
@@ -13,7 +13,7 @@ export default function Projects() {
             Проекты
           </h1>
           <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
-            Тест
+            test
           </p>
         </div>
         <div className="container py-12">
