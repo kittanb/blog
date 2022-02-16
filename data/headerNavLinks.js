0876f524e27ed82c2b0a8ebbdf7155d755515642
kittanb <vbiziukov@gmail.com>
2022-02-16 08:46:06 +0300

    test #3 logo

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -1,8 +1,8 @@
 const headerNavLinks = [
   { href: '/blog', title: 'Блог' },
-//  { href: '/tags', title: 'Tags' },
+  { href: '/tags', title: 'Теги' },
 //  { href: '/projects', title: 'Projects' },
-//  { href: '/about', title: 'About' },
+  { href: '/about', title: 'Обо мне' },
 ]
 
 export default headerNavLinks
