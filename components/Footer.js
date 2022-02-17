80a779b56f3973dab1710cdb80799920db650ab1
Kittan Bachika <vbiziukov@gmail.com>
2022-02-18 02:59:13 +0300

    Update Footer.js
    juan

--- components/Footer.js
+++ components/Footer.js
@@ -23,8 +23,7 @@ export default function Footer() {
         </div>
         <div className="mb-8 text-sm text-gray-500 dark:text-gray-400">
           <Link href="https://github.com/kittanb/blog">
-            Блог для всякого
-            
+            Juan.            
           </Link>
         </div>
       </div>
