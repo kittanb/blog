f557501d239d78733ba136fac78b7697ee1d758d
Kittan Bachika <vbiziukov@gmail.com>
2022-02-18 05:26:50 +0300

    Update siteMetadata.js

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -52,7 +52,7 @@ const siteMetadata = {
       // transparent_dark, preferred_color_scheme, custom
       theme: 'dark',
       // theme when dark mode
-      darkTheme: 'preferred_color_scheme',
+      darkTheme: 'transparent_dark',
       // If the theme option above is set to 'custom`
       // please provide a link below to your custom theme css file.
       // example: https://giscus.app/themes/custom_example.css
