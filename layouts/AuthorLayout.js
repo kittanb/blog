2b3532ee44d5bc72cd11ae3b8b330663b861491d
kittanb <vbiziukov@gmail.com>
2022-02-21 12:28:52 +0300

    build fix #3

--- layouts/AuthorLayout.js
+++ layouts/AuthorLayout.js
@@ -3,7 +3,7 @@ import Image from '@/components/Image'
 import { PageSEO } from '@/components/SEO'
 
 export default function AuthorLayout({ children, frontMatter }) {
-  const {name, avatar, occupation, company, email, twitter, linkedin, github, skype, telegram}=frontMatter
+  const { name, avatar, occupation, company, email, telegram, skype, github } = frontMatter
 
   return (
     <>
